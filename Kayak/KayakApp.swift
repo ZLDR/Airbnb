e35780a5236e9b45027f4c3b4bcc5bb76f3fcf0a
Dorian Zlatan <zlatan.dorian272@gmail.com>
2021-05-23 22:48:42 +0300

    Initial Commit

---
+++ Kayak/KayakApp.swift
@@ -0,0 +1,17 @@
+//
+//  KayakApp.swift
+//  Kayak
+//
+//  Created by Dorian ZLATAN on 23.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct KayakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
